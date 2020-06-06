39a6e05ce42e62332a4f80bb93f8263b01b97c75
michaelb87 <mb@michaelb.at>
2020-06-06 23:15:06 +0200

    refactored core framework for clarity

--- lib/ssh_parser.ex
+++ lib/ssh_parser.ex
@@ -10,9 +10,9 @@ defmodule SshParser do
     eval_modules(modules, str)
   end
 
-  defp eval_modules([module|remaining_modules], str) do
-    case apply(elem(module, 1), :parse, [str]) do
-      {:ok, [result], _, _, _, _} -> {elem(module, 0), result}
+  defp eval_modules([{module_type, module} | remaining_modules], str) do
+    case apply(module, :parse, [str]) do
+      {:ok, [result], _, _, _, _} -> {module_type, result}
       {:error, _, _, _, _, _} -> eval_modules(remaining_modules, str)
     end
   end
